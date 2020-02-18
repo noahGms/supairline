d1d7c51ecad95c67f1fe395ff765bed53793f642
Noah <40141862+xNoah@users.noreply.github.com>
2020-02-18 16:32:26 +0100

    Layout template

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top text-center">
+    <div class="row">
+        <div class="col-12 col-md">
+            <img class="mb-2" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+            <small class="d-block mb-3 text-muted">© 2019 | SupAirLine tout droit résérver</small>
+        </div>
+    </div>
+</footer>
